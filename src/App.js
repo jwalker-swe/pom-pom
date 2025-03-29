b2487e901ac828836f605ef5d565e87ca5771f1a
PRISM-EXE <jwalker.swe@gmail.com>
2025-03-29 00:53:18 -0400

    Feat: icons change along with state

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import Btn from './components/Button.js';
 import './styles/App.css';
 import { GlobalStateProvider, GlobalStateContext } from './GlobalState';
 
-const playBtnLogo = process.env.PUBLIC_URL + "/images/icons/ph_play-fill.png";
-const settingsBtnLogo = process.env.PUBLIC_URL + '/images/icons/ph_dots-three-outline-fill.png';
-const skipBtnLogo = process.env.PUBLIC_URL + '/images/icons/ph_fast-forward-fill.png';
+let backgroundColor = 'rgba(255, 76, 76, 0.15)';
+let mainBtnColor = 'rgba(255, 76, 76, 0.71)';
+let secondaryBtnColor = 'rgba(255, 76, 76, 0.15)'
+let fontColor = '#471515';
+let chipBorderColor = '#471515';
 
+let playBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_play-fill.png';
+let settingsBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_dots-three-outline-fill.png';
+let skipBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_fast-forward-fill.png';
 
-let backgroundColor;
-let mainBtnColor;
-let secondaryBtnColor;
-let fontColor;
+let chipLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_brain-fill.png';
+
+let duration = (10);
 
 
 function App() {
@@ -26,10 +30,25 @@ function App() {
 
   const setColor = () => {
     if ( globalState.mode === 'focus' ) {
-        backgroundColor = 'rgba(255, 76, 76, 0.15)'
+        backgroundColor = 'rgba(255, 76, 76, 0.15)';
         mainBtnColor = 'rgba(255, 76, 76, 0.71)';
         secondaryBtnColor = 'rgba(255, 76, 76, 0.15)'
         fontColor = '#471515';
+
+        if ( globalState.running == true ) {
+          playBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_play-fill.png';
+        } else {
+          playBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_pause-fill.png';
+        }
+          settingsBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_dots-three-outline-fill.png';
+          skipBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_fast-forward-fill.png';
+
+          chipLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_brain-fill.png';
+          
+
+        chipBorderColor = '#471515';
+
+        duration = (10);
     }
 
     if ( globalState.mode === 'short-break' ) {
@@ -37,6 +56,28 @@ function App() {
         mainBtnColor = 'rgba(77, 218, 110, 0.62)';
         secondaryBtnColor = 'rgba(77, 218, 110, 0.15)';
         fontColor = '#14401D';
+
+        playBtnLogo = process.env.PUBLIC_URL + '/images/icons/short-break/ph_play-fill.png';
+        settingsBtnLogo = process.env.PUBLIC_URL + '/images/icons/short-break/ph_dots-three-outline-fill.png';
+        skipBtnLogo = process.env.PUBLIC_URL + '/images/icons/short-break/ph_fast-forward-fill.png';
+
+        chipLogo = process.env.PUBLIC_URL + '/images/icons/short-break/ph_coffee.png';
+
+        chipBorderColor = '#14401D';
+
+        duration = (5);
+    }
+  }
+
+  function changeIcon() {
+    if ( globalState.running == false ) {
+      if ( globalState.mode === 'focus' ) {
+        playBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_pause-fill.png';
+      }
+    } else {
+      if ( globalState.mode === 'focus' ) {
+        playBtnLogo = process.env.PUBLIC_URL + '/images/icons/focus/ph_play-fill.png';
+      };
     }
   }
 
@@ -44,12 +85,16 @@ function App() {
     setColor();
   }, [globalState.mode])
 
+  useEffect ( () => {
+    changeIcon();
+  }, [globalState.running])
+
 
   return (
       <div className="App" style={{backgroundColor: backgroundColor}}>
         <div className="Canvas">
-          <Chip backgroundColor={backgroundColor} fontColor={fontColor}/>
-          <Timer duration={10} fontColor={fontColor} />
+          <Chip backgroundColor={backgroundColor} fontColor={fontColor} chipLogo={chipLogo} borderColor={chipBorderColor}/>
+          <Timer duration={duration} fontColor={fontColor} />
           <div className='btns-container'>
             <Btn btnType={'settings'} btnLogo={settingsBtnLogo} bgColor={secondaryBtnColor} btnPadding={'10px 20px'}/>
             <Btn btnType={'play-pause'} btnLogo={playBtnLogo} bgColor={mainBtnColor} btnPadding={'15px 30px'}/>
--- src/components/Chip.js
+++ src/components/Chip.js
@@ -8,16 +8,32 @@ const iconStateShortBreak = process.env.PUBLIC_URL + '/images/icons/state/icon-s
 const iconStateLongBreak = process.env.PUBLIC_URL + '/images/icons/state/icon-state-long-break.png';
 
 function Chip(props) {
-    return (
-        <div className="container chip-main-container" style={{backgroundColor: props.backgroundColor}}>
-            <div className='chip-secondary-container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <div className="chip-icon-container" style={{width: 32, height: 32, marginRight: 8}}>
-                    <img className="focus-icon" src={iconStateFocus} style={{width: 28, height: 28}}></img>
+
+    const { globalState, setGlobalState } = useContext(GlobalStateContext);
+
+    if ( globalState.mode === 'focus' ) {
+        return (
+            <div className="container chip-main-container" style={{backgroundColor: props.backgroundColor, borderColor: props.borderColor}}>
+                <div className='chip-secondary-container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <div className="chip-icon-container" style={{width: 32, height: 32, marginRight: 8}}>
+                        <img className="focus-icon" src={props.chipLogo} style={{width: 28, height: 28}}></img>
+                    </div>
+                    <h1 className="heading chip-heading" style={{color: props.fontColor}}>Focus</h1>
                 </div>
-                <h1 className="heading chip-heading" style={{color: props.fontColor}}>Focus</h1>
             </div>
-        </div>
-    );
+        );
+    } else if ( globalState.mode === 'short-break' ) {
+        return (
+            <div className="container chip-main-container" style={{backgroundColor: props.backgroundColor, borderColor: props.borderColor}}>
+                <div className='chip-secondary-container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <div className="chip-icon-container" style={{width: 32, height: 32, marginRight: 8}}>
+                        <img className="focus-icon" src={props.chipLogo} style={{width: 28, height: 28}}></img>
+                    </div>
+                    <h1 className="heading chip-heading" style={{color: props.fontColor}}>Short Break</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Chip;
